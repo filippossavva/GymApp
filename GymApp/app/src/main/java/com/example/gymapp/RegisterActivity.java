a1c0f08713d6b6e7883287c28202d4b689ef180f
makis97 <61313949+makis97@users.noreply.github.com>
2020-11-14 00:39:58 +0200

    half validations register

--- GymApp/app/src/main/java/com/example/gymapp/RegisterActivity.java
+++ GymApp/app/src/main/java/com/example/gymapp/RegisterActivity.java
@@ -51,7 +51,6 @@ public class RegisterActivity extends AppCompatActivity {
         group = (RadioGroup) findViewById(R.id.rbGroup);
         register = (Button) findViewById(R.id.button_register);
 
-        validate();
 
         databaseReference = FirebaseDatabase.getInstance().getReference("user");
         fAuth = FirebaseAuth.getInstance();
@@ -78,74 +77,79 @@ public class RegisterActivity extends AppCompatActivity {
                 }
 
 
-                loading.setVisibility(View.VISIBLE);
-                fAuth.createUserWithEmailAndPassword(emailText,passwordText).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful())
-                        {
-                            user information = new user(
-                                    nameText,
-                                    surnameText,
-                                    emailText,
-                                    usernameText,
-                                    passwordText,
-                                    gender
-                            );
-                            FirebaseDatabase.getInstance().getReference("user")
-                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                    .setValue(information).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    Toast.makeText(RegisterActivity.this, "Register Successful", Toast.LENGTH_LONG).show();
-                                    Intent in = new Intent(getApplicationContext(), RegisterActivity.class);
-                                    startActivity(in);
-                                    EditText e1 = findViewById(R.id.etName);
-                                    EditText e2 = findViewById(R.id.etSurname);
+                if(validateName() && validateSurname() && validateUsername() && validateEmail() && validatePassword() && validateConfPass()){
+                    loading.setVisibility(View.VISIBLE);
+                    fAuth.createUserWithEmailAndPassword(emailText,passwordText).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful())
+                            {
+                                user information = new user(
+                                        nameText,
+                                        surnameText,
+                                        emailText,
+                                        usernameText,
+                                        passwordText,
+                                        gender
+                                );
+                                FirebaseDatabase.getInstance().getReference("user")
+                                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                        .setValue(information).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        Toast.makeText(RegisterActivity.this, "Register Successful", Toast.LENGTH_LONG).show();
+                                        Intent in = new Intent(getApplicationContext(), RegisterActivity.class);
+                                        startActivity(in);
+                                        EditText e1 = findViewById(R.id.etName);
+                                        EditText e2 = findViewById(R.id.etSurname);
 
-                                    String name = e1.getText().toString().trim();
-                                    String surname = e2.getText().toString().trim();
-
-
-                                    NotificationChannel channel = null;
-                                    if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                                        channel = new NotificationChannel(
-                                                "1",
-                                                "channel1",
-                                                NotificationManager.IMPORTANCE_DEFAULT);
-
-                                        //create the notification manager
-                                        NotificationManager manager = getSystemService(NotificationManager.class);
-                                        manager.createNotificationChannel(channel);
-
-                                        //create the notification
+                                        String name = e1.getText().toString().trim();
+                                        String surname = e2.getText().toString().trim();
 
 
-                                        NotificationCompat.Builder notification = new NotificationCompat.Builder(RegisterActivity.this, "1")
-                                                .setSmallIcon(android.R.drawable.btn_star)
-                                                .setContentTitle("Congratulations " +name+ " "+surname+ "!" +"\n You are ready to log in")
-                                                .setContentText(body)
-                                                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                                        NotificationChannel channel = null;
+                                        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                            channel = new NotificationChannel(
+                                                    "1",
+                                                    "channel1",
+                                                    NotificationManager.IMPORTANCE_DEFAULT);
 
-                                        NotificationManagerCompat notifyAdmin = NotificationManagerCompat.from(RegisterActivity.this);
-                                        notifyAdmin.notify(1, notification.build());
+                                            //create the notification manager
+                                            NotificationManager manager = getSystemService(NotificationManager.class);
+                                            manager.createNotificationChannel(channel);
+
+                                            //create the notification
+
+
+                                            NotificationCompat.Builder notification = new NotificationCompat.Builder(RegisterActivity.this, "1")
+                                                    .setSmallIcon(android.R.drawable.btn_star)
+                                                    .setContentTitle("Congratulations " +name+ " "+surname+ "!" +"\n You are ready to log in")
+                                                    .setContentText(body)
+                                                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                                            NotificationManagerCompat notifyAdmin = NotificationManagerCompat.from(RegisterActivity.this);
+                                            notifyAdmin.notify(1, notification.build());
+                                        }
                                     }
-                                }
-                            });
+                                });
 
+                            }
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(), "Error: " + task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                            }
                         }
-                        else
-                        {
-                            Toast.makeText(getApplicationContext(), "Error: " + task.getException().getMessage(),Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
+                    });
+                }
+
+
             }
         });
 
 
 
     }
+
 //    public void sendNotification(View v) {
 //        EditText e1 = findViewById(R.id.etName);
 //        EditText e2 = findViewById(R.id.etSurname);
@@ -183,56 +187,143 @@ public class RegisterActivity extends AppCompatActivity {
 //        }
 //    }
 
-    public boolean validate()
+    public boolean validateName()
     {
-        name = (EditText) findViewById(R.id.etName);
-        surname = (EditText) findViewById(R.id.etSurname);
-        username = (EditText) findViewById(R.id.etEmailAddress);
-        email = (EditText) findViewById(R.id.etEmail);
-        password = (EditText) findViewById(R.id.etPassword);
-        confPass = (EditText) findViewById(R.id.etConfirmPass);
-        loading = (ProgressBar) findViewById(R.id.pbLoading);
-        group = (RadioGroup) findViewById(R.id.rbGroup);
-        register = (Button) findViewById(R.id.button_register);
-        String nameText = name.getText().toString().trim();
-        String surnameText = surname.getText().toString().trim();
-        String usernameText = username.getText().toString().trim();
-        String emailText = email.getText().toString().trim();
-        String passwordText = password.getText().toString().trim();
-        String confirmPasswordText = confPass.getText().toString().trim();
+        EditText nameText = findViewById(R.id.etName);
+        String val = nameText.getText().toString().trim();
+        if (val.isEmpty()) {
+            nameText.setError("Name can not be empty");
+            return false;
+        } else {
+            nameText.setError(null);
+            return true;
+        }
+    }
+    public boolean validateSurname()
+    {
+        EditText surnameText = findViewById(R.id.etSurname);
+        String val = surnameText.getText().toString().trim();
+        if (val.isEmpty()) {
+            surnameText.setError("Surname can not be empty");
+            return false;
+        } else {
+            surnameText.setError(null);
+            return true;
+        }
+    }
+    public boolean validateUsername()
+    {
+        EditText usernameText = findViewById(R.id.etEmailAddress);
+        String val = usernameText.getText().toString().trim();
+        int count = 0;
 
-        boolean valid = true;
-        if(nameText.isEmpty())
+        for(int i=0 ; i<val.length();i++)
         {
-            name.setError("Name is Required.");
-            valid = false;
+            if (Character.isDigit(val.charAt(i)))
+            {
+                count++;
+            }
         }
-        if(surnameText.isEmpty())
+        if(count == 0)
         {
-            surname.setError("Surname is Required.");
-            valid = false;
+            usernameText.setError("Username must contains at least one number");
+            return false;
         }
-        if(usernameText.isEmpty())
+        else if (val.isEmpty()) {
+            usernameText.setError("Username can not be empty");
+            return false;
+        } else if (val.length() > 20) {
+            usernameText.setError("Username is too large");
+            return false;
+        }else if (val.length() < 6) {
+            usernameText.setError("Username must contains up to 6 characters");
+            return false;
+        }else {
+            usernameText.setError(null);
+            return true;
+        }
+    }
+    private boolean validateEmail() {
+        EditText emailText = findViewById(R.id.etEmail);
+        String val = emailText.getText().toString().trim();
+        if (val.isEmpty()) {
+            emailText.setError("Email can not be empty");
+            return false;
+        } else if (val.contains("@")) {
+            emailText.setError("Email must contains @");
+            return false;
+        }else {
+            emailText.setError(null);
+            return true;
+        }
+    }
+    private boolean validatePassword() {
+        EditText passwordText = findViewById(R.id.etPassword);
+        String val = passwordText.getText().toString().trim();
+        int count = 0;
+
+        for(int i=0 ; i<val.length();i++)
         {
-            username.setError("Username is Required.");
-            valid = false;
+            if (Character.isDigit(val.charAt(i)))
+            {
+                count++;
+            }
         }
-        if(emailText.isEmpty())
+        if(count == 0)
         {
-            email.setError("Email is Required.");
-            valid = false;
+            passwordText.setError("Password must contains at least one number");
+            return false;
         }
-        if(passwordText.isEmpty())
+
+        if (val.isEmpty()) {
+            passwordText.setError("Password can not be empty");
+            return false;
+        }else if (val.length() < 6) {
+            passwordText.setError("Password must contains up to 6 characters");
+            return false;
+        }
+        else if (val.length() > 30) {
+            passwordText.setError("Password is too large");
+            return false;
+        }
+            else {
+            passwordText.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateConfPass()
+    {
+        EditText passwordText = findViewById(R.id.etPassword);
+        String val1 = passwordText.getText().toString().trim();
+        EditText confPassText = findViewById(R.id.etConfirmPass);
+        String val2 = passwordText.getText().toString().trim();
+
+        int count = 0;
+
+        for(int i=0 ; i<val1.length();i++)
         {
-            password.setError("Password is Required.");
-            valid = false;
+            if (Character.isDigit(val1.charAt(i)) )
+            {
+                count++;
+            }
         }
-        if(!confirmPasswordText.equals(passwordText))
+        if(count == 0)
         {
-            confPass.setError("Passwords do not match.");
-            valid = false;
+            passwordText.setError("Password must contains at least one number");
+            return false;
         }
-        return valid;
+
+        if(val1 != val2)
+        {
+            confPassText.setError("Passwords do not match. Please try again");
+            return false;
+        }
+        else
+        {
+            confPassText.setError(null);
+            return true;
+        }
     }
     public void goToLogin(View v)
     {
