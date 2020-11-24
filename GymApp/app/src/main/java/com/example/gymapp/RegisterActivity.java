2324f65f37fe4b2c3f3919aa83ba022fcd97c3f4
makis97 <61313949+makis97@users.noreply.github.com>
2020-11-24 23:57:30 +0200

    SAVING STATE REGISTER

--- GymApp/app/src/main/java/com/example/gymapp/RegisterActivity.java
+++ GymApp/app/src/main/java/com/example/gymapp/RegisterActivity.java
@@ -20,6 +20,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -32,15 +33,24 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+
 public class RegisterActivity extends AppCompatActivity {
     EditText name,surname,username,email,password,confPass;
     Button register;
     RadioGroup group;
+    RadioButton male,female;
     ProgressBar loading;
     DatabaseReference databaseReference;
     FirebaseAuth fAuth;
     String gender = "";
     private CharSequence body;
+    private String file = "gym2lift.txt";
+
 
 
     @Override
@@ -56,8 +66,50 @@ public class RegisterActivity extends AppCompatActivity {
         confPass = (EditText) findViewById(R.id.etConfirmPass);
         loading = (ProgressBar) findViewById(R.id.pbLoading);
         group = (RadioGroup) findViewById(R.id.rbGroup);
+        male = (RadioButton) findViewById((R.id.rbMale));
+        female = (RadioButton) findViewById((R.id.rbFemale));
         register = (Button) findViewById(R.id.button_register);
 
+        try {
+            FileInputStream fin = openFileInput(file); //find file
+            DataInputStream din = new DataInputStream(fin);
+            InputStreamReader isr = new InputStreamReader(din);
+            BufferedReader br = new BufferedReader(isr);
+
+            int i=0;
+            String lines[] = new String [7];
+            String strLine;
+
+            //read from buffered reader and save items into string line by line
+            while((strLine = br.readLine()) != null)
+            {
+                lines[i] = strLine;
+                i++;
+            }
+
+            name.setText(lines[0]);
+            surname.setText(lines[1]);
+            email.setText(lines[2]);
+            username.setText(lines[3]);
+            password.setText(lines[4]);
+            confPass.setText(lines[5]);
+            String gender = (lines[6]);
+            int selection = Integer.parseInt(gender);
+            if(selection == R.id.rbMale)
+            {
+                male.setChecked(true);
+            }
+            else if(selection == R.id.rbFemale)
+            {
+                female.setChecked(true);
+            }
+
+            fin.close();
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
 
         databaseReference = FirebaseDatabase.getInstance().getReference("user");
         fAuth = FirebaseAuth.getInstance();
@@ -91,6 +143,17 @@ public class RegisterActivity extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if(task.isSuccessful())
                             {
+                                try
+                                {
+                                    FileOutputStream fout = openFileOutput(file, 0);
+                                    fout.write("".getBytes());
+                                    fout.close();
+                                }
+                                catch (Exception ex)
+                                {
+                                    ex.printStackTrace();
+                                }
+
                                 user information = new user(
                                         nameText,
                                         surnameText,
@@ -167,42 +230,6 @@ public class RegisterActivity extends AppCompatActivity {
 
     }
 
-//    public void sendNotification(View v) {
-//        EditText e1 = findViewById(R.id.etName);
-//        EditText e2 = findViewById(R.id.etSurname);
-//
-//        String name = e1.getText().toString().trim();
-//        String surname = e2.getText().toString().trim();
-//
-//
-//        NotificationChannel channel = null;
-//        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            channel = new NotificationChannel(
-//                    "1",
-//                    "channel1",
-//                    NotificationManager.IMPORTANCE_DEFAULT);
-//
-//            //create the notification manager
-//            NotificationManager manager = getSystemService(NotificationManager.class);
-//            manager.createNotificationChannel(channel);
-//
-//            //create the notification
-//
-//
-//            NotificationCompat.Builder notification = new NotificationCompat.Builder(this, "1")
-//                    .setSmallIcon(android.R.drawable.btn_star)
-//                    .setContentTitle("Congratulations!You are ready to log in")
-//                    .setContentText(body)
-//                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-//
-//            NotificationManagerCompat notifyAdmin = NotificationManagerCompat.from(this);
-//            notifyAdmin.notify(1, notification.build());
-//        }
-//        else {
-//
-//
-//        }
-//    }
 
     public boolean validateName()
     {
@@ -322,6 +349,46 @@ public class RegisterActivity extends AppCompatActivity {
     }
     public void goToLogin(View v)
     {
+        EditText name = findViewById(R.id.etName);
+        String nameData = name.getText().toString() + "\n";
+
+        EditText surname = findViewById(R.id.etSurname);
+        String surnameData = surname.getText().toString() + "\n";
+
+        EditText email = findViewById(R.id.etEmail);
+        String emailData = email.getText().toString() + "\n";
+
+        EditText username = findViewById(R.id.etEmailAddress);
+        String usernameData = username.getText().toString() + "\n";
+
+        EditText password = findViewById(R.id.etPassword);
+        String passwordData = password.getText().toString() + "\n";
+
+        EditText confpassword = findViewById(R.id.etConfirmPass);
+        String confpasswordData = confpassword.getText().toString() + "\n";
+
+        RadioGroup gender = findViewById(R.id.rbGroup);
+        int choice = gender.getCheckedRadioButtonId();
+        String genderData = choice + "\n";
+
+        try {
+            FileOutputStream fout = openFileOutput(file,0);
+            fout.write(nameData.getBytes());
+            fout.write(surnameData.getBytes());
+            fout.write(emailData.getBytes());
+            fout.write(usernameData.getBytes());
+            fout.write(passwordData.getBytes());
+            fout.write(confpasswordData.getBytes());
+            fout.write(genderData.getBytes());
+            fout.close();
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+
+
+
         Intent in = new Intent(this, LoginActivity.class);
         startActivity(in);
     }
